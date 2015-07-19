fdd9ae583c1aaed27bc30770601e94b882431dd8
Andrew Block <andy.block@gmail.com>
2015-07-18 21:25:07 -0500

    Updated script to update OSE custom application

--- ose-update-src-app-url.sh
+++ ose-update-src-app-url.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
 
-#
-# HOST
-# USER
-# PASSWORD
-# PROJECT
-# APP
-
-
 
 usage() {
-  echo "Usage $0 -h|--host=\"<host>\" -u|--user=\"<username>\" -p|--password=\"<password>\" -n|--namespace=\"<namespace>\" -a|--app=\"<app>\" -v|--version=\"<version>\""
+  echo "Usage $0 -h|--host=\"<host>\" -u|--user=\"<username>\" -p|--password=\"<password>\" -n|--namespace=\"<namespace>\" -a|--app=\"<app>\" -s|--source=\"<source>\""
 }
 
-
-# Check program dependencies
-
-command -v jq -q >/dev/null 2>&1 || { echo >&2 "json parser jq is required but not installed yet... aborting."; exit 1; }
 
 
 # Process Input
@@ -40,14 +28,14 @@ do
     -a=*|--app=*)
       APP="${i#*=}"
       shift;;
-    -v=*|--version=*)
-      VERSION="${i#*=}"
+    -s=*|--source=*)
+      SOURCE="${i#*=}"
       shift;;
   esac
 done
 
-if [ -z $HOST ] || [ -z $USER ] || [ -z $PASSWORD ] || [ -z $NAMESPACE ] || [ -z $APP ]; then
-  echo "Missing required args"
+if [ -z $HOST ] || [ -z $USER ] || [ -z $PASSWORD ] || [ -z $NAMESPACE ] || [ -z $APP ] || [ -z $SOURCE ]; then
+  echo "Missing required arguments!"
   usage
   exit 1
 fi 
@@ -57,11 +45,9 @@ fi
 CHALLENGE_RESPONSE=$(curl -s  -I --insecure -f  "https://${HOST}:8443/oauth/authorize?response_type=token&client_id=openshift-challenging-client" --user ${USER}:${PASSWORD} -H "X-CSRF-Token: 1")
 
 if [ $? -ne 0 ]; then
-    echo "Unauthorized Access Attempt"
+    echo "Error: Unauthorized Access Attempt"
     exit 1
 fi
-
-echo "Authenticated"
 
 TOKEN=$(echo "$CHALLENGE_RESPONSE" | grep -oP "access_token=\K[^&]*")
 
@@ -69,7 +55,6 @@ if [ -z "$TOKEN" ]; then
     echo "Token is blank!"
     exit 1
 fi
-
 
 # Get build config for app
 BUILD_CONFIG=$(curl -s -H "Authorization: Bearer ${TOKEN}" --insecure -f https://${HOST}:8443/osapi/v1beta3/namespaces/${NAMESPACE}/buildconfigs/${APP})
@@ -79,21 +64,11 @@ if [ -z "$BUILD_CONFIG" ]; then
     exit 1
 fi
 
-echo "Located build config"
-
-# Determine current version
-CURRENT_VERSION=$(echo "$BUILD_CONFIG" | jq -r '.spec.strategy.customStrategy.env[] as $cusenv | select($cusenv.name == "ARTIFACT_VERSION") | $cusenv.value | .')
-
-if [ -z "$CURRENT_VERSION" ]; then
-    echo "Unable to determine current version of artifact"
-    exit 1
-fi
-
+# Cleanse Source for sed
+CLEANSED_SOURCE=$(echo $SOURCE | sed 's/[_&$]/\\&/g')
 
 # Update version of artifact
-UPDATED_BUILD_CONFIG=$(echo "$BUILD_CONFIG" | sed "s/\"value\": \"$CURRENT_VERSION\"/\"value\": \"$VERSION\"/")
-
-
+UPDATED_BUILD_CONFIG=$(echo "$BUILD_CONFIG" | sed "s|\"value\": \"http.*|\"value\": \"$CLEANSED_SOURCE\"|")
 
 # Update buildconfig in OSE
 curl -s -X PUT -d "${UPDATED_BUILD_CONFIG}" -H "Authorization: Bearer ${TOKEN}" --insecure -f https://${HOST}:8443/osapi/v1beta3/namespaces/${NAMESPACE}/buildconfigs/${APP} > /dev/null
@@ -103,4 +78,5 @@ if [  $? -ne 0  ]; then
     exit 1
 fi
 
-echo "Project version has been updated!"
+echo
+echo "Application source has been updated!"
