935e1dc9920bfcd39e1c2a8ec2dd84ada86e68d2
Andrew Block <andy.block@gmail.com>
2015-08-23 22:58:45 -0500

    Added new script to trigger and monitor build and updated update app url script

--- ose-update-src-app-url.sh
+++ ose-update-src-app-url.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
+PORT=8443
 
 usage() {
-  echo "Usage $0 -h|--host=\"<host>\" -u|--user=\"<username>\" -p|--password=\"<password>\" -n|--namespace=\"<namespace>\" -a|--app=\"<app>\" -s|--source=\"<source>\" -t|--token=\"<token>\""
+  echo "Usage $0 -h|--host=\"<host>\" -p|--port=\"<port>\" -u|--user=\"<username>\" -w|--password=\"<password>\" -n|--namespace=\"<namespace>\" -a|--app=\"<app>\" -s|--source=\"<source>\" -t|--token=\"<token>\""
 }
 
 
@@ -16,10 +17,13 @@ do
     -h=*|--host=*)
       HOST="${i#*=}"
       shift;;
+    -p=*|--port=*)
+      PORT="${i#*=}"
+      shift;;
     -u=*|--user=*)
       USER="${i#*=}"
       shift;;
-    -p=*|--password=*)
+    -w=*|--password=*)
       PASSWORD="${i#*=}"
       shift;;
     -n=*|--namespace=*)
@@ -37,7 +41,7 @@ do
   esac
 done
 
-if [ -z $HOST ] || [ -z $USER ] || [ -z $NAMESPACE ] || [ -z $APP ] || [ -z $SOURCE ]; then
+if [ -z $HOST ] || [ -z $NAMESPACE ] || [ -z $APP ] || [ -z $SOURCE ]; then
   echo "Missing required arguments!"
   usage
   exit 1
@@ -51,9 +55,16 @@ fi
 
 # Get token if not present
 if [ -z $TOKEN ]; then
+	
+	# Validate user and password are present
+	if [ -z $PASSWORD ] && [ -z $USER ]; then
+		echo "Username and Password must be provided"
+		usage
+		exit 1
+	fi
 
 	# Get auth token
-	CHALLENGE_RESPONSE=$(curl -s  -I --insecure -f  "https://${HOST}:8443/oauth/authorize?response_type=token&client_id=openshift-challenging-client" --user ${USER}:${PASSWORD} -H "X-CSRF-Token: 1")
+	CHALLENGE_RESPONSE=$(curl -s  -I --insecure -f  "https://${HOST}:${PORT}/oauth/authorize?response_type=token&client_id=openshift-challenging-client" --user ${USER}:${PASSWORD} -H "X-CSRF-Token: 1")
 
 	if [ $? -ne 0 ]; then
 	    echo "Error: Unauthorized Access Attempt"
@@ -70,7 +81,7 @@ if [ -z $TOKEN ]; then
 fi
 
 # Get build config for app
-BUILD_CONFIG=$(curl -s -H "Authorization: Bearer ${TOKEN}" --insecure -f https://${HOST}:8443/osapi/v1beta3/namespaces/${NAMESPACE}/buildconfigs/${APP})
+BUILD_CONFIG=$(curl -s -H "Authorization: Bearer ${TOKEN}" --insecure -f https://${HOST}:${PORT}/osapi/v1beta3/namespaces/${NAMESPACE}/buildconfigs/${APP})
 
 if [ -z "$BUILD_CONFIG" ]; then
     echo "Error locating build config"
---
+++ trigger-monitor-build.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+set -e
+
+
+PORT=8443
+BUILD_PHASE=""
+COUNTER=0
+DELAY=20
+MAX_COUNTER=30
+BUILD_NAME=
+
+
+# Validate JQ is installed
+command -v jq -q >/dev/null 2>&1 || { echo >&2 "json parser jq is required but not installed yet... aborting."; exit 1; }
+
+usage() {
+  echo "Usage $0 -h|--host=\"<host>\" -p|--port=\"<port>\" -u|--user=\"<username>\" -w|--password=\"<password>\" -n|--namespace=\"<namespace>\" -a|--app=\"<app>\" -t|--token=\"<token>\""
+}
+
+# Set Trap
+trap end_trap EXIT
+
+
+# Process Input
+for i in "$@"
+do
+  case $i in
+    -h=*|--host=*)
+      HOST="${i#*=}"
+      shift;;
+    -p=*|--port=*)
+      PORT="${i#*=}"
+      shift;;
+    -u=*|--user=*)
+      USER="${i#*=}"
+      shift;;
+    -w=*|--password=*)
+      PASSWORD="${i#*=}"
+      shift;;
+    -n=*|--namespace=*)
+      NAMESPACE="${i#*=}"
+      shift;;
+    -a=*|--app=*)
+      APP="${i#*=}"
+      shift;;
+    -t=*|--token=*)
+      TOKEN="${i#*=}"
+      shift;;
+  esac
+done
+
+
+function end_trap() {
+
+  if [ $? -eq 0 ]; then
+	echo
+	echo "====================================================="  
+	echo "= ${APP} Build ${BUILD_NAME} Succeeded!        "
+	echo "====================================================="  
+	echo
+  else
+  	echo
+  	echo "====================================================="  
+  	echo "= ${APP} Build Failed!                              ="
+  	echo "====================================================="  
+  	echo
+fi
+
+}
+
+
+# Get token if not present
+if [ -z $TOKEN ]; then
+	
+	# Validate user and password are present
+	if [ -z $PASSWORD ] && [ -z $USER ]; then
+		echo "Username and Password must be provided"
+		usage
+		exit 1
+	fi
+
+	# Get auth token
+	CHALLENGE_RESPONSE=$(curl -s  -I --insecure -f  "https://${HOST}:8443/oauth/authorize?response_type=token&client_id=openshift-challenging-client" --user ${USER}:${PASSWORD} -H "X-CSRF-Token: 1")
+
+	if [ $? -ne 0 ]; then
+	    echo "Error: Unauthorized Access Attempt"
+	    exit 1
+	fi
+
+
+	TOKEN=$(echo "$CHALLENGE_RESPONSE" | grep -oP "access_token=\K[^&]*")
+
+	if [ -z "$TOKEN" ]; then
+    	echo "Token is blank!"
+    	exit 1
+	fi
+fi
+
+# Trigger a new build of application
+echo "Triggering new build of ${APP}..."
+
+
+NEW_BUILD_REQUEST=$(curl -s -f -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -X POST --data-binary "{\"kind\":\"BuildRequest\",\"apiVersion\":\"v1beta3\",\"metadata\":{\"name\":\"$APP\"}}" --insecure  https://${HOST}:${PORT}/osapi/v1beta3/namespaces/${NAMESPACE}/buildconfigs/${APP}/instantiate)
+
+BUILD_NAME=$(echo $NEW_BUILD_REQUEST | jq -r .metadata.name)
+
+echo
+echo "New build created: $BUILD_NAME"
+
+
+
+
+# Allow build to progress through lifecycle (pending -> running). Give the build X amount of time to complete each phase resetting the counter
+# when a new phase is entered
+
+while [ $COUNTER -lt $MAX_COUNTER ]
+do
+	
+	BUILD_STATUS_RESPONSE=$(curl -s -f -H "Authorization: Bearer ${TOKEN}" --insecure  https://${HOST}:${PORT}/osapi/v1beta3/namespaces/${NAMESPACE}/builds/${BUILD_NAME})
+	
+	BUILD_STATUS=$(echo $BUILD_STATUS_RESPONSE | jq -r .status.phase)
+	
+	
+	# Check if build succeeded or failed
+	if [ "$BUILD_STATUS" == "Success" ]; then
+		echo
+		break
+	fi
+	
+	if [ "$BUILD_STATUS" == "Failed" ] || [ "$BUILD_STATUS" == "Failed" ]; then
+		echo
+		echo
+		echo "Build ${BUILD_NAME} Failed with status \"${BUILD_STATUS}\""
+		exit 1
+	fi
+	
+	# Check build phase
+	if [ "$BUILD_STATUS" != "$BUILD_PHASE" ]; then
+		echo
+		echo
+		echo "Build Phase Changed. \"${BUILD_PHASE}\" -> \"${BUILD_STATUS}\""
+		BUILD_PHASE=$BUILD_STATUS
+		
+		echo -n "Build Status: \"${BUILD_STATUS}\" "
+		
+		# Reset Counter
+		COUNTER=0
+	fi
+	
+	echo -n "."
+	COUNTER=$(( $COUNTER + 1 ))
+	
+	sleep $DELAY
+done
